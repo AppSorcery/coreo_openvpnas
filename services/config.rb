c8344b8710adba8915eddecb61936e2e1eb4d649
Adam Taylor <ataylor@adam-taylors-macbook-pro.local>
2016-03-21 19:22:39 -0700

    'solo_run'

--- services/config.rb
+++ services/config.rb
@@ -20,8 +20,8 @@ coreo_aws_vpc_routetable "${PRIVATE_ROUTE_NAME}" do
   vpc "${VPC_NAME}"
   number_of_tables 3
   tags [
-    "Name=${PRIVATE_ROUTE_NAME}"
-  ]
+        "Name=${PRIVATE_ROUTE_NAME}"
+       ]
 end
 
 coreo_aws_vpc_subnet "${PRIVATE_SUBNET_NAME}" do
