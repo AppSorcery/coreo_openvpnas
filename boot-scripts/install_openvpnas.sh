ca061ab6aa7aa3ed0d5f324989019cc7e91f1449
Adam Taylor <ataylor@adam-taylors-macbook-pro.local>
2016-03-21 21:58:22 -0700

    'solo_run'

--- boot-scripts/install_openvpnas.sh
+++ boot-scripts/install_openvpnas.sh
@@ -3,3 +3,5 @@
 echo "Red Hat Enterprise Linux Server release 6.3 (Santiago)" > /etc/redhat-release
 wget http://swupdate.openvpn.org/as/openvpn-as-2.0.25-CentOS6.x86_64.rpm
 rpm -i ./openvpn-as-2.0.5-CentOS6.x86_64.rpm
+
+# /usr/local/openvpn_as/bin/ovpn-init
