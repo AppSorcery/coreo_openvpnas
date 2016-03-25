e2ec0d2e2b05111434dc2e1e0bb9f4cca020f6b6
Adam Taylor <adam@appsorcery.com>
2016-03-24 17:43:45 -0700

    changes to use udp

--- boot-scripts/install_openvpnas.sh
+++ boot-scripts/install_openvpnas.sh
@@ -17,8 +17,15 @@ rpm -ivh http://swupdate.openvpn.org/as/openvpn-as-2.0.25-CentOS6.x86_64.rpm
 
 cd /usr/local/openvpn_as/etc/db
 
+# http://sites.inka.de/~bigred/devel/tcp-tcp.html
+
 if [ -z "${VPN_PROTO:-}" ]; then
-    VPN_PROTO="tcp"
+    VPN_PROTO="udp"
+fi
+
+PORT_SHARE_ENABLE=false
+if [ "$VPN_PROTO" == "tcp" ]
+    PORT_SHARE_ENABLE=true
 fi
 
 if [ -z "${VPN_PORT:-}" ]; then
@@ -29,10 +36,6 @@ if [ -z "${HTTPS_PORT:-}" ]; then
     HTTPS_PORT=443
 fi
 
-if [ -z "${ALLOW_UDP:-}" ]; then
-    ALLOW_UDP=false
-fi
-
 service openvpnas stop
 
 sqlite3 config.db "UPDATE config SET value='$VPN_DNS_PREFIX.$DNS_ZONE' WHERE name='host.name'";
@@ -40,9 +43,11 @@ sqlite3 config.db "UPDATE config SET value='$VPN_DNS_PREFIX.$DNS_ZONE' WHERE nam
 sqlite3 config.db "UPDATE config SET value='$VPN_PROTO' WHERE name='vpn.daemon.0.listen.protocol'";
 sqlite3 config.db "UPDATE config SET value='$VPN_PORT' WHERE name='vpn.daemon.0.listen.port'";
 sqlite3 config.db "UPDATE config SET value='$VPN_PORT' WHERE name='vpn.server.daemon.tcp.port'";
-sqlite3 config.db "UPDATE config SET value='$ALLOW_UDP' WHERE name='vpn.server.daemon.enable'";
+sqlite3 config.db "UPDATE config SET value='false' WHERE name='vpn.server.daemon.enable'";
 
 sqlite3 config.db "UPDATE config SET value='$HTTPS_PORT' WHERE name='admin_ui.https.port'";
 sqlite3 config.db "UPDATE config SET value='$HTTPS_PORT' WHERE name='cs.https.port'";
+sqlite3 config.db "UPDATE config SET value='$PORT_SHARE_ENABLE' WHERE name='vpn.server.port_share.enable'";
+
 
 service openvpnas start
